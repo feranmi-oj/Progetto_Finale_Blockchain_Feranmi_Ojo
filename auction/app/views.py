bf883914ec9d8aa9b09474ae77327abb6202071c
Feranmi Ojo <gabbo.ojo28@gmail.com>
2022-02-14 13:51:35 +0100

    various modifications

--- auction/app/views.py
+++ auction/app/views.py
@@ -7,16 +7,16 @@ from django.views.generic.edit import CreateView
 from .models import Profile,Shoes
 from django.contrib import messages
 import json
-from django.utils import timezone
-from  django.urls import reverse
-# Create your views here.
 
+
+#import Database Redis
 client = redis.StrictRedis(host='127.0.0.1', port='6379')
 
-
+#views first page app
 def home_page_view(request):
     return render(request,'app/base.html')
 
+#views create auctions shoes
 class Create_object_sell_view( CreateView):
     model = Shoes
     fields = "name", "image", "description", "starting_price", "buy_now", "end_auction"
@@ -25,14 +25,18 @@ class Create_object_sell_view( CreateView):
 
 
 
-
+#views show shoes
 def show_object_sell_view(request, pk):
     profile = Profile.objects.get(user=request.user)
     shoes = get_object_or_404(Shoes, pk=pk)
     context = {'shoes': shoes}
     utc = pytz.UTC
+    #if the auction ends, it rewards the winner if one exists
     if shoes.end_auction.replace(tzinfo=utc) < datetime.today().replace(tzinfo=utc):
         if shoes.buyer!= None:
+            #generates a JSON file containing all the details of the auction and references to the winner.
+            # Then calculate the hash of this JSON and write it in a transaction on the Ethereum (Ropsten) blockchain.
+
             winner_auction = {
                 'Winner': f'{shoes.buyer.user}',
                 'Final Shoes Price': f'{shoes.buy_now}',
@@ -49,14 +53,15 @@ def show_object_sell_view(request, pk):
         shoes.save()
     return render(request, 'app/show_section.html', context)
 
+
 def home_auction_view(request):
 
-    shoes = Shoes.objects.filter(status='open')
+    shoes = Shoes.objects.filter(status='open').order_by('-end_auction')
     context = {'shoes': shoes}
     return render(request, 'app/home_auction.html', context)
 
 
-
+#views make offer
 def make_offer_view(request,pk):
     profile = Profile.objects.get(user=request.user)
     shoes = get_object_or_404(Shoes, pk=pk)
@@ -70,6 +75,7 @@ def make_offer_view(request,pk):
                 messages.error(request, 'This Offer Cannot Be Updated, Because Is Lower Than 0 $')
                 return redirect('app:make_an_offer', kwargs={'pk': shoes.pk})
             if profile.usd_balance >= offer:
+                #if the offer is equal to the cost of the shoes to buy now the auction ends and the user wins the shoes
                 if offer == shoes.buy_now:
                     if shoes.offer > 0.0:
                         prev_offer = shoes.offer
@@ -79,9 +85,7 @@ def make_offer_view(request,pk):
                     profile.usd_balance = profile.usd_balance - shoes.buy_now
                     shoes.buyer = profile
                     profile.save()
-                    messages.success(request,
-                                     """Your offer is equal to Buy Now price. Congrats To Your Wonderfull Item Choise. Your Balance Now Is """ + str(
-                                         profile.usd_balance) + ' $')
+                    messages.success(request,f"""Your offer is equal to Buy Now price. Congrats To Your Wonderfull Item Choise. Your Balance Now Is {profile.usd_balance} $""" )
                     prev_buyer.usd_balance += float(shoes.buy_now)
                     shoes.status = 'close'
                     shoes.save()
@@ -96,7 +100,7 @@ def make_offer_view(request,pk):
                     winJson = json.dumps(winner_auction)
                     # shoes.write_on_chain(winJson)
                     return redirect('app:home_auction')
-
+                #if the bid is greater than the initial price and the previous bid, the user wins the auction for the time being
                 elif offer >= shoes.starting_price and offer >= shoes.offer:
                     prev_offer = shoes.offer
 
@@ -134,6 +138,7 @@ def make_offer_view(request,pk):
         form = MakeAnOffer()
     return render(request, 'app/make_an_offer.html', {'form': form,'shoes': shoes})
 
+#views buy now
 def buy_now(request):
     profile = Profile.objects.get(user=request.user)
     for shoes in Shoes.objects.filter().order_by('-end_auction'):
@@ -151,7 +156,7 @@ def buy_now(request):
             shoes.status='close'
             shoes.save()
 
-#ROPSTEN Here
+            #ROPSTEN Here
             winner_buy_now= {
                 'Winner': f'{shoes.buyer.user}',
                 'Final Shoes Price': f'{shoes.buy_now}',
